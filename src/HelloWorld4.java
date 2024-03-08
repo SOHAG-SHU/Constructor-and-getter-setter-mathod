589a7e01121355b31593e084ca79677dcd050a5b
SOHAG-SHU <sohag209366@gmail.com>
2024-03-09 00:57:34 +0600

    getter setter mathod

---
+++ src/HelloWorld4.java
@@ -0,0 +1,48 @@
+public class HelloWorld4{
+    private int roll;
+    private String name;
+    private boolean passed;
+    public HelloWorld4(){ /* Constructor */
+        roll = 100;
+        name = "CSE";
+        passed = true;
+    }
+    public HelloWorld4(int roll, String name, boolean passed){ /* Constructor */
+        this.roll = roll;
+        this.name = name;
+        this.passed = passed;
+    }
+
+    public void setRoll(int roll) {
+        this.roll = roll;
+    }
+
+    public void setPassed(boolean passed) {
+        this.passed = passed;
+    }
+
+    public int getRoll() {
+        return roll;
+    }
+
+    public boolean isPassed() {
+        return passed;
+    }
+
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public static void main(String[] args) {
+        HelloWorld4 helloWorld = new HelloWorld4();
+        HelloWorld4 helloWorld2 = new HelloWorld4(300,"SHU", true);
+        helloWorld.setName("ICE");
+        helloWorld.setRoll(400);
+        helloWorld.setPassed(false);
+        System.out.println(helloWorld2.getName());
+        System.out.println(helloWorld.getRoll());
+        System.out.println(helloWorld2.isPassed());
+    }
+}
